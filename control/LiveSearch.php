278f3bc3bec84c27794f3bc0de7b329480b38b4d
Yannick94 <yst1994@gmx.ch>
2019-01-14 11:32:19 +0100

    Placed in the Correct Space

--- control/LiveSearch.php
+++ control/LiveSearch.php
@@ -1,4 +1,41 @@
 <?php
+
+class LiveSearch
+{
+    private $db;
+    private $mysqli;
+    
+    public function __construct()
+    {
+        $this->db     = Database::getInstance();
+        $this->mysqli = $this->db->getConnection();
+        $this->mysqli->set_charset('utf-8');
+    }
+
+    public function searchForProduct($searchString){
+      $returnArray = array();
+
+        $stmt = $this->mysqli->prepare('SELECT * FROM product WHERE TitleDe like "%?% OR TitleEn like "%?%" OR DescriptionDe like "%?%" OR DescriptionEn like "%?%"');
+        $stmt->bind_param('ssss',$searchString,$searchString,$searchString,$searchString);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        while($row = $result->fetch_assoc()){
+            $prod = new Product();
+            $prod->setId($row["Id"]);
+            $prod->setTitleDe($row["TitleDe"]);
+            $prod->setTitleEn($row["TitleEn"]);
+            $prod->setDescriptionDe($row["DescriptionDe"]);
+            $prod->setDescriptionEn($row["DescriptionEn"]);
+            $prod->setPrice($row["Price"]);
+            $prod->setCategorieId($row["CategorieId"]);
+            $prod->setPicturePath($row["PicturePath"]);
+            array_push($returnArray,$prod);
+        }
+
+        return $returnArray;
+    }
+  }
+
 $xmlDoc=new DOMDocument();
 $xmlDoc->load("links.xml"); // need to setup this xml file with all products with <title> tags
 
