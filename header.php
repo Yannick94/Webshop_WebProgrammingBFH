73e226fa887f7b4a70861f1a80f6ee1b41071207
Yannick94 <yst1994@gmx.ch>
2019-01-14 11:23:35 +0100

    Moved it to corret position

--- header.php
+++ header.php
@@ -47,30 +47,6 @@
 <head>
     <title><?php getContent('seiteTitel'); ?></title>
     <link rel="shortcut icon" type="image/x-icon" href="\res\pic\Logo.ico" />
-    <script>
-     // livesearch function
-     function showResult(str) {
-        if (str.length==0) { 
-          document.getElementById("livesearch").innerHTML="";
-          document.getElementById("livesearch").style.border="0px";
-          return;
-        }
-        if (window.XMLHttpRequest) {
-          // code for IE7+, Firefox, Chrome, Opera, Safari
-          xmlhttp=new XMLHttpRequest();
-        } else {  // code for IE6, IE5
-          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function() {
-          if (this.readyState==4 && this.status==200) {
-            document.getElementById("livesearch").innerHTML=this.responseText;
-            document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-          }
-        }
-        xmlhttp.open("GET","livesearch.php?q="+str,true);
-        xmlhttp.send();
-      }
-    </script>
 </head>
 
 <body>
