bac8d6be19510fc88e169bb61fa93dfc3a62f5a9
Yannick94 <yst1994@gmx.ch>
2019-01-14 11:14:05 +0100

    Added Mainpage and fixed some Errors in Javascript

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
+if(isset($_POST["1"])){
+    header("Location: /Overview?cat=1");
+}
+if(isset($_POST["2"])){
+    header("Location: /Overview?cat=2");
+}
+if(isset($_POST["3"])){
+    header("Location: /Overview?cat=3");
+}
+if(isset($_POST["4"])){
+    header("Location: /Overview?cat=4");
+}
+
 include('header.php');
+    echo '<form method="post" action="">';
+    echo '<div class="Column">';
+    echo '<div class="HardwareMain"><button class="MainPageNav" name="1" id="1" type="submit"><i class="fa fa-desktop fa-2x"></i><i>Hardware</i></button></div>';
+    echo '<div class="PeripherieMain"><button class="MainPageNav" name="2" id="2" type="submit"><i class="fa fa-print fa-2x"></i><i>Peripherie</i></button></div>';
+    echo '</div>';
+    echo '<div class="Column">';
+    echo '<div class="SoftwareMain"><button class="MainPageNav" name="3" id="3" type="submit"><i class="fa fa-cogs fa-2x"></i><i>Software</i></button></div>';
+    echo '<div class="AudioMain"><button class="MainPageNav" name="4" id="4" type="submit"><i class="fa fa-headphones fa-2x"></i><i>Audio</i></button></div>';
+    echo '</div>';
+    echo '</form>';
 include('footer.php');
 ?>
--- js/index.js
+++ js/index.js
@@ -1,221 +1,225 @@
 $(document).ready(function() {
 
-            var menuOpen = false;
+    var menuOpen = false;
 
-            var userOpen = false;
-            $('.Navigation').on('mousedown touchstart', function() {
+    var userOpen = false;
+    $('.Navigation').on('mousedown touchstart', function() {
 
-                if (!menuOpen) $(this).find('.Hardware').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(105px,30px)'
-                });
-                else $(this).find('.Hardware').css({
-                    'background-color': 'dimGray',
-                    'transform': 'none'
-                });
-                if (!menuOpen) $(this).find('.Peripherie').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(75px,75px)'
-                });
-                else $(this).find('.Peripherie').css({
-                    'background-color': 'darkGray',
-                    'transform': 'none'
-                });
-                if (!menuOpen) $(this).find('.Software').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(30px,105px)'
-                });
-                else $(this).find('.Software').css({
-                    'background-color': 'silver',
-                    'transform': 'none'
-                });
-                if (!menuOpen) $(this).find('.Audio').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(-20px,125px)'
-                });
-                else $(this).find('.Audio').css({
-                    'background-color': 'silver',
-                    'transform': 'none'
-                });
-                if (!menuOpen) $(this).find('.Home').css({
-                    'background-color': 'gray',
-                    'transform': 'translate(125px,-20px)'
-                });
-                else $(this).find('.Home').css({
-                    'background-color': 'silver',
-                    'transform': 'none'
-                });
-                menuOpen = !menuOpen;
+        if (!menuOpen) $(this).find('.Hardware').css({
+            'background-color': 'gray',
+            'transform': 'translate(105px,30px)'
+        });
+        else $(this).find('.Hardware').css({
+            'background-color': 'dimGray',
+            'transform': 'none'
+        });
+        if (!menuOpen) $(this).find('.Peripherie').css({
+            'background-color': 'gray',
+            'transform': 'translate(75px,75px)'
+        });
+        else $(this).find('.Peripherie').css({
+            'background-color': 'darkGray',
+            'transform': 'none'
+        });
+        if (!menuOpen) $(this).find('.Software').css({
+            'background-color': 'gray',
+            'transform': 'translate(30px,105px)'
+        });
+        else $(this).find('.Software').css({
+            'background-color': 'silver',
+            'transform': 'none'
+        });
+        if (!menuOpen) $(this).find('.Audio').css({
+            'background-color': 'gray',
+            'transform': 'translate(-20px,125px)'
+        });
+        else $(this).find('.Audio').css({
+            'background-color': 'silver',
+            'transform': 'none'
+        });
+        if (!menuOpen) $(this).find('.Home').css({
+            'background-color': 'gray',
+            'transform': 'translate(125px,-20px)'
+        });
+        else $(this).find('.Home').css({
+            'background-color': 'silver',
+            'transform': 'none'
+        });
+        menuOpen = !menuOpen;
+    });
+
+    var catParam = new URLSearchParams(window.location.search);
+    resetActive();
+    if (catParam.has('cat')) {
+        var param = catParam.get('cat');
+        if (param == 1) {
+            $('#NavHardware').css({
+                'color': '#ff0000'
             });
-
-            $('#NavHome').click(function(){
-                resetActive();
-                $('#NavHome').css({
-                    'color': '#ff0000'
-                });
-                if(menuOpen){
-                    closeMenu();
-                    setTimeout(function (){ 
-                        window.location = "/";
-                    }, 1000);
-                }else{
-                    window.location = "/";
-                }
+        }
+        if (param == 2) {
+            $('#NavPeripherie').css({
+                'color': '#ff0000'
             });
-
-            $('#NavAudio').click(function() {
-                resetActive();
-                $('#NavAudio').css({
-                    'color': '#ff0000'
-                });
-                if(menuOpen){
-                    closeMenu();
-                    setTimeout(function (){ 
-                        window.location = "/Overview?cat=4";
-                    }, 1000);
-                }else{
-                    window.location = "/Overview?cat=4";
-                }
+        }
+        if (param == 3) {
+            $('#NavSoftware').css({
+                'color': '#ff0000'
             });
-
-            $('#NavSoftware').click(function() {
-                resetActive();
-                $('#NavSoftware').css({
-                    'color': '#ff0000'
-                });
-                if(menuOpen){
-                    closeMenu();
-                    setTimeout(function (){ 
-                        window.location = "/Overview?cat=3";
-                    }, 1000);
-                }else{
-                    window.location = "/Overview?cat=3";
-                }
+        }
+        if (param == 4) {
+            $('#NavAudio').css({
+                'color': '#ff0000'
             });
+        }
+    } else {
+        $('#NavHome').css({
+            'color': '#ff0000'
+        });
+    }
 
-            $('#NavPeripherie').click(function() {
-                resetActive();
-                $('#NavPeripherie').css({
-                    'color': '#ff0000'
-                });
-                if(menuOpen){
-                    closeMenu();
-                    setTimeout(function (){ 
-                        window.location = "/Overview?cat=2";
-                    }, 1000);
-                }else{
-                    window.location = "/Overview?cat=2";
-                }
-            });
+    $('#NavHome').click(function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/";
+            }, 1000);
+        } else {
+            window.location = "/";
+        }
+    });
 
-            $('#NavHardware').click(function() {
-                resetActive();
-                $('#NavHardware').css({
-                    'color': '#ff0000'
-                });
-                if(menuOpen){
-                    closeMenu();
-                    setTimeout(function (){ 
-                        window.location = "/Overview?cat=1";
-                    }, 1000);
-                }else{
-                    window.location = "/Overview?cat=1";
-                }
-            });
+    $('#NavAudio').click(function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Overview?cat=4";
+            }, 1000);
+        } else {
+            window.location = "/Overview?cat=4";
+        }
+    });
 
-            $('#NavHome').click(function() {
-                resetActive();
-                $('#NavHome').css({
-                    'color': '#ff0000'
-                });
-            });
+    $('#NavSoftware').click(function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Overview?cat=3";
+            }, 1000);
+        } else {
+            window.location = "/Overview?cat=3";
+        }
+    });
 
-            function resetActive() {
-                $('#NavHome').css({
-                    'color': '#fff'
-                });
-                $('#NavHardware').css({
-                    'color': '#fff'
-                });
-                $('#NavPeripherie').css({
-                    'color': '#fff'
-                });
-                $('#NavSoftware').css({
-                    'color': '#fff'
-                });
-                $('#NavAudio').css({
-                    'color': '#fff'
-                });
-            }
+    $('#NavPeripherie').click(function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Overview?cat=2";
+            }, 1000);
+        } else {
+            window.location = "/Overview?cat=2";
+        }
+    });
 
-            function closeMenu(){
-                $(this).find('.Hardware').css({
-                    'background-color': 'dimGray',
-                    'transform': 'none'});
-                $(this).find('.Peripherie').css({
-                        'background-color': 'darkGray',
-                        'transform': 'none'
-                });
-                $(this).find('.Software').css({
-                    'background-color': 'silver',
-                    'transform': 'none'
-                });
-                $(this).find('.Audio').css({
-                    'background-color': 'silver',
-                    'transform': 'none'
-                });
-                $(this).find('.Home').css({
-                    'background-color': 'silver',
-                    'transform': 'none'
-                });
-                menuOpen = false;
-            }
+    $('#NavHardware').click(function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Overview?cat=1";
+            }, 1000);
+        } else {
+            window.location = "/Overview?cat=1";
+        }
+    });
 
-                $('#User').on('mousedown touchstart', function() {
-                    if(menuOpen){
-                        closeMenu();
-                        setTimeout(function (){ 
-                            window.location = "/Login";
-                        }, 1000);
-                    }else{
-                        window.location = "/Login";
-                    }
-                })
+    function resetActive() {
+        $('#NavHome').css({
+            'color': '#fff'
+        });
+        $('#NavHardware').css({
+            'color': '#fff'
+        });
+        $('#NavPeripherie').css({
+            'color': '#fff'
+        });
+        $('#NavSoftware').css({
+            'color': '#fff'
+        });
+        $('#NavAudio').css({
+            'color': '#fff'
+        });
+    }
 
-                $('#Cart').on('mousedown touchstart', function(){
-                    if(menuOpen){
-                        closeMenu();
-                        setTimeout(function(){
-                            window.location = "/Cart";
-                        }, 1000);
-                    }else{
-                        window.location = "/Cart";
-                    }
-                })
+    function closeMenu() {
+        $(this).find('.Hardware').css({
+            'background-color': 'dimGray',
+            'transform': 'none'
+        });
+        $(this).find('.Peripherie').css({
+            'background-color': 'darkGray',
+            'transform': 'none'
+        });
+        $(this).find('.Software').css({
+            'background-color': 'silver',
+            'transform': 'none'
+        });
+        $(this).find('.Audio').css({
+            'background-color': 'silver',
+            'transform': 'none'
+        });
+        $(this).find('.Home').css({
+            'background-color': 'silver',
+            'transform': 'none'
+        });
+        menuOpen = false;
+    }
 
-                $(".MainContent").on('mousedown touchstart', '#registerLoginbtn', function(){
-                    if(menuOpen){
-                        closeMenu();
-                        setTimeout(function (){ 
-                            window.location = "/Register";
-                        }, 1000);
-                    }else{
-                        window.location = "/Register";
-                    }
-                })
+    $('#User').on('mousedown touchstart', function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Login";
+            }, 1000);
+        } else {
+            window.location = "/Login";
+        }
+    })
 
-                $(".MainContent").on('mousedown touchstart','.cancelLoginbtn', function(){
-                    if(menuOpen){
-                        closeMenu();
-                        setTimeout(function (){ 
-                            window.location = "/";
-                        }, 1000);
-                    }else{
-                        window.location = "/";
-                    }
-                })
+    $('#Cart').on('mousedown touchstart', function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Cart";
+            }, 1000);
+        } else {
+            window.location = "/Cart";
+        }
+    })
 
-                $(".clickForProduct").click(function() {
-                    window.location = "/Product?Id="+$(this).data("id");
-                });
-            });
+    $(".MainContent").on('mousedown touchstart', '#registerLoginbtn', function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/Register";
+            }, 1000);
+        } else {
+            window.location = "/Register";
+        }
+    })
+
+    $(".MainContent").on('mousedown touchstart', '.cancelLoginbtn', function() {
+        if (menuOpen) {
+            closeMenu();
+            setTimeout(function() {
+                window.location = "/";
+            }, 1000);
+        } else {
+            window.location = "/";
+        }
+    })
+
+    $(".clickForProduct").click(function() {
+        window.location = "/Product?Id=" + $(this).data("id");
+    });
+});
